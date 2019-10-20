feb2666432e805de2e85b458c0874751aa2c0861
Elton Cheung <eltoncheung@dhcp-wifi-8021x-155-41-1-18.bu.edu>
2019-10-20 15:29:51 -0400

    added forecast

--- PS4/routes/ps4.js
+++ PS4/routes/ps4.js
@@ -6,13 +6,13 @@ const fetch = require('node-fetch');
 router.get('/', async function(req, res, next) {
     const weather = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=0731f3195c156e62e35cd545e2a1cea5&units=imperial');
     const weatherJson = await weather.json();
-
-    console.log(weatherJson);
+    console.log(weatherJson)
     res.render('ps4',
       {title:'Here is the current weather in Boston!',
        longitude: weatherJson.coord.lon,
        latitude: weatherJson.coord.lat,
-       temperature: weatherJson.main.temp
+       temperature: weatherJson.main.temp,
+       forecast: weatherJson.weather[0].description
        });
 });
 
